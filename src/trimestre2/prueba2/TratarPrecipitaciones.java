d01e053b17e6f0b2288eaff0df973881912b6cf2
cherre1 <carloscherrero@yahoo.es>
2022-03-07 11:08:54 +0100

    Agregando clase TratarPrecipitaciones con esqueleto de métodos

---
+++ src/trimestre2/prueba2/TratarPrecipitaciones.java
@@ -0,0 +1,23 @@
+package trimestre2.prueba2;
+
+public class TratarPrecipitaciones {
+	
+	static final int MESES=12;
+
+	public static int[] obtenerPrecipitacionAnual (int[][] precipitaciones) {
+		return null;
+	}
+	
+	public static int[] obtenerPromedioMensual (int[][] precipitaciones) {
+		return null;
+	}
+	
+	public static int[] obtenerMesesExtremos (int[][] precipitaciones, int localidad) {
+		return null;
+	}
+	
+	public static int[][] obtenerMesesExtremos (int[][] precipitaciones) {
+		return null;
+	}
+
+}
